fc754ad89f5d530a7cb50a6ef5428b7754138eba
mishael254 <mishaelmishantoz@gmail.com>
2024-02-13 17:02:33 +0300

    learning react

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      backgroundColor:"lightblue",
+      flex:1,
+      flexDirection:"row",
+      justifyContent:"center",
+      alignItems:"center"
+    }}>
+        <View style={{
+          backgroundColor:"orange",
+          
+          width:100,
+          height:300
+        }}
+        />
+        <View style = {{
+          backgroundColor:"indigo",
+          
+          width:100,
+          height:200
+        }}
+        />
+        <View style = {{
+          backgroundColor:"lightgreen",
+          
+          width:100,
+          height:100
+        }}/>
+
+
     </View>
   );
 }
